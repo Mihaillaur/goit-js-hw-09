521e08f1b5bd537ec16f19f249ffbbaeba3ca766
mihaillaur <mihaillaur879@gmail.com>
2023-08-18 01:26:51 +0300

    hw-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,32 @@
-console.log(hello);
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+const body = document.querySelector("body");
+
+let bodyBcgColor = null;
+stopBtn.disabled = true;
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+startBtn.addEventListener("click", handlerClickStart);
+function handlerClickStart(evt){
+    bodyBcgColor = setInterval(() => {
+        body.style.backgroundColor =  getRandomHexColor();
+      }, 1000);
+      startBtn.disabled = true;
+      stopBtn.disabled = false;
+    
+};
+stopBtn.addEventListener("click", handlerClickStop);
+function handlerClickStop(evt){
+    clearInterval(bodyBcgColor);
+    startBtn.disabled = false;
+      stopBtn.disabled = true;
+};
 
 
 
 
 
-// function getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-//   }
+
+
